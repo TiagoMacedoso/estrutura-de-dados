b3512e2a29b824d7ebecc67c666b584220e4ad40
Tiago Macedo Cardoso <contato.cardosotiago@gmail.com>
2024-03-26 19:02:34 -0300

    Adição do código da aula do dia 12/03

---
+++ 0312_tupla_dicionarios/exemplos_tupla.py
@@ -0,0 +1,16 @@
+numerosLista = [1,2,3,4,5] ##mutável
+
+numerosTupla = (1,2) ##imutável
+
+print(numerosLista)
+print(numerosTupla)
+
+print(type(numerosLista))
+print(type(numerosTupla))
+
+tupla2 = (10,11,12,13)
+
+##Concatenação
+tupla3 = tupla2 + numerosTupla
+
+print(tupla3)
